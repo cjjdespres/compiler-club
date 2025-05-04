cf124e04c57500a87a752e30135745bc017596a9
Christian Despres <christian.j.j.despres@gmail.com>
2025-05-04 17:40:53 -0400

    Add stack VM

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,11 +1,11 @@
 mod simple;
 
-use simple::{ast, interpreter, parse, pretty};
+use simple::{ast, interpreter, parse, pretty, stack_vm};
 
 pub fn run_examples() {
     println!("First calculator example:");
     println!("{}", pretty::print(ast::example_1()));
-    println!("This evaluates to:");
+    println!("Our interpreter says this is:");
     println!("{}", interpreter::interpret(ast::example_1()));
     println!("We can even parse stuff!");
     let mut example_str = "2 + 2";
@@ -14,4 +14,9 @@ pub fn run_examples() {
         "{}",
         interpreter::interpret(parse::parse(&mut example_str).unwrap())
     );
+    let stack_eval = stack_vm::eval(parse::parse(&mut example_str).unwrap());
+    println!(
+        "And our stack VM works too, because {} is {}",
+        example_str, stack_eval
+    )
 }
--- src/calculator/simple.rs
+++ src/calculator/simple.rs
@@ -351,3 +351,138 @@ pub mod interpreter {
         }
     }
 }
+
+// A little stack VM as a compilation target for our calculator programs
+pub mod stack_vm {
+    use super::ast::Expr;
+
+    // Our VM can:
+    // 1. pop two numbers off the stack and push their sum onto the stack
+    // 2. pop two numbers off the stack and push their product onto the stack
+    // 3. push an integer constant onto the stack
+    #[derive(Debug, PartialEq)]
+    pub enum Instr {
+        Add,
+        Mul,
+        Const(i64),
+    }
+
+    pub struct VM {
+        stack: Vec<i64>,
+    }
+
+    // Like all good VMs, we simply declare that it is UB to attempt to execute
+    // an instr against a stack that isn't in the correct state for it. In
+    // practice, that means we can be lazy and panic whenever anything might go
+    // wrong.
+    //
+    // We'd never generate an incorrect program, obviously, so that disclaimer
+    // is irrelevant.
+    impl VM {
+        pub fn new() -> Self {
+            Self { stack: Vec::new() }
+        }
+
+        pub fn pop(&mut self) -> i64 {
+            self.stack.pop().unwrap()
+        }
+
+        pub fn push(&mut self, num: i64) {
+            self.stack.push(num)
+        }
+
+        pub fn step(&mut self, instr: Instr) {
+            match instr {
+                Instr::Add => {
+                    let x = self.pop();
+                    let y = self.pop();
+                    self.push(x + y)
+                }
+                Instr::Mul => {
+                    let x = self.pop();
+                    let y = self.pop();
+                    self.push(x * y)
+                }
+                Instr::Const(num) => self.push(num),
+            }
+        }
+
+        pub fn execute_all(&mut self, instrs: Vec<Instr>) {
+            for instr in instrs {
+                self.step(instr)
+            }
+        }
+    }
+
+    pub fn execute(instrs: Vec<Instr>) -> VM {
+        let mut vm = VM::new();
+        vm.execute_all(instrs);
+        vm
+    }
+
+    // Compile the given expression and add the instructions to the accumulator.
+    // The result is a program that does whatever the accumulator did, then
+    // additionally pushes the result of evaluating the expression onto the
+    // stack.
+    fn compile_onto(expr: Expr, instrs: &mut Vec<Instr>) {
+        match expr {
+            Expr::Add(x, y) => {
+                // For addition, we just need a program that computes x, then
+                // computes y, then executes a single Add.
+                compile_onto(*x, instrs);
+                compile_onto(*y, instrs);
+                instrs.push(Instr::Add)
+            }
+            Expr::Mul(x, y) => {
+                // Same as Add
+                compile_onto(*x, instrs);
+                compile_onto(*y, instrs);
+                instrs.push(Instr::Mul)
+            }
+            Expr::Lit(num) => instrs.push(Instr::Const(num)),
+        }
+    }
+
+    pub fn compile(expr: Expr) -> Vec<Instr> {
+        let mut instrs = Vec::new();
+        compile_onto(expr, &mut instrs);
+        instrs
+    }
+
+    pub fn eval(expr: Expr) -> i64 {
+        let mut vm = execute(compile(expr));
+        vm.pop()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::super::{ast, interpreter};
+        use super::{compile, eval, Instr};
+
+        #[test]
+        fn test_example_1_compiles() {
+            assert_eq!(
+                compile(ast::example_1()),
+                vec![
+                    Instr::Const(3),
+                    Instr::Const(5),
+                    Instr::Mul,
+                    Instr::Const(-7),
+                    Instr::Const(11),
+                    Instr::Add,
+                    Instr::Const(9),
+                    Instr::Mul,
+                    Instr::Add
+                ]
+            )
+        }
+
+        #[test]
+        fn test_example_1_runs_like_interpreter() {
+            assert_eq!(
+                eval(ast::example_1()),
+                interpreter::interpret(ast::example_1())
+            )
+        }
+    }
+}
